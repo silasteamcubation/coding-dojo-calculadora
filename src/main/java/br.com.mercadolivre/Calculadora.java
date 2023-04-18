1dbfb801e09ae8194e8a707fa5687446045daec4
Alexia Calmon <alexia.calmon@mercadolivre.com>
2023-04-18 16:29:42 -0300

    Adicionando subtração e divisão

--- src/main/java/br.com.mercadolivre/Calculadora.java
+++ src/main/java/br.com.mercadolivre/Calculadora.java
@@ -2,7 +2,16 @@ package br.com.mercadolivre;
 
 public class Calculadora {
 
-     public double soma(double a, double b) {
+    public double soma(double a, double b) {
         return a + b;
+    }
+
+
+    public double subtracao(double a, double b) {
+        return a - b;
+    }
+
+    public double divisao(double a, double b) {
+        return a/b;
     }
 }
--- src/test/java/br.com.mercadolivre/CalculadoraTest.java
+++ src/test/java/br.com.mercadolivre/CalculadoraTest.java
@@ -12,5 +12,20 @@ public class CalculadoraTest {
         assertEquals(5, resultado);
     }
 
+    @Test
+    public void testeSubtracao() {
 
+        Calculadora calculadora = new Calculadora();
+        double resultado = calculadora.subtracao(2,3);
+        assertEquals(-1, resultado);
+    }
+
+    @Test
+    public void testeDivisao() {
+
+        Calculadora calculadora = new Calculadora();
+        double resultado = calculadora.divisao(6,2);
+        assertEquals(3, resultado);
+
+    }
 }
